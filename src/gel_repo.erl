a2a2e16378978fc8b5dfc12ddced30d7b3f147f3
Bogdan Kalyta <bogdadiys@gmail.com>
2019-04-03 14:53:55 +0300

    Added: result matching

--- src/gel_repo.erl
+++ src/gel_repo.erl
@@ -13,28 +13,34 @@ init(Repository) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     {ok, Url} = gel_hub:new(Repository),
     Path      = Dir ++ Repository,
-    ok = filelib:ensure_dir(Path),
-    [10,48,10 | _] = lists:reverse(os:cmd("cd "++ Path ++ 
-        " && git init && echo yes | git remote add origin " ++ Url ++ 
-        " && echo $?")),
-    ok = file:write_file(Path ++ "/README.MD", list_to_binary(Repository)),
-    {ok, _}   = commit(Repository, "initial commit"),
-    [10,48,10 | _] = lists:reverse(os:cmd("cd " ++ Path ++ 
-        " && git push -u origin master && echo $?")),
-    [10,48,10 | _] = lists:reverse(os:cmd("cd " ++ Path ++ 
-        " && git checkout -b devel && git push -u origin devel && echo $?")),
-    {ok, list_to_binary(Url)}.
+    case file:make_dir(Path) of ->
+        ok ->
+            [10,48,10 | _] = lists:reverse(os:cmd("cd "++ Path ++ 
+                " && git init && echo yes | git remote add origin " ++ Url ++ 
+                " && echo $?")),
+            ok = file:write_file(Path++"/README.MD",list_to_binary(Repository)),
+            {ok, _}   = commit(Repository, "initial commit"),
+            [10,48,10 | _] = lists:reverse(os:cmd("cd " ++ Path ++ 
+                " && git push -u origin master && echo $?")),
+            [10,48,10 | _] = lists:reverse(os:cmd("cd " ++ Path ++ " && git "
+                "checkout -b devel && git push -u origin devel && echo $?")),
+            {ok, list_to_binary(Url)};
+        _ ->
+            {error, 'name already exists on this account'}
+    end.
 
 pull(Repository) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ " && git pull && echo $?")),
+    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ 
+        " && git pull && echo $?")),
     {ok, lists:reverse(Result)}.
 
 push(Repository) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ " && git push && echo $?")),
+    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ 
+        " && git push && echo $?")),
     {ok, lists:reverse(Result)}.
 
 commit(Repository, Commit) ->
@@ -47,18 +53,21 @@ commit(Repository, Commit) ->
 checkout(Repository, Branch) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ " && git checkout " ++ Branch ++ " && echo $?")),
+    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ 
+        " && git checkout " ++ Branch ++ " && echo $?")),
     {ok, lists:reverse(Result)}.
 
 merge(Repository, Branch) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    [10,48,10 | Result] = lists:reverse(("cd " ++ Path ++ " && git merge " ++ Branch ++ " && echo $?")),
+    [10,48,10 | Result] = lists:reverse(("cd " ++ Path ++ 
+        " && git merge " ++ Branch ++ " && echo $?")),
     {ok, _}   = push(Repository),
     {ok, lists:reverse(Result)}.
 
 log(Repository) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ " && git log && echo $?")),
+    [10,48,10 | Result] = lists:reverse(os:cmd("cd " ++ Path ++ 
+        " && git log && echo $?")),
     {ok, lists:reverse(Result)}.
