afbc84731215f8532ebb4c09d42f48f6939aae9d
Bogdan Kalyta <bogdadiys@gmail.com>
2019-04-09 13:21:14 +0300

    Added: file loading in checkout function

--- src/gel_repo.erl
+++ src/gel_repo.erl
@@ -53,7 +53,45 @@ commit(Repository, Commit) ->
 checkout(Repository, Branch) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
     Path      = Dir ++ Repository,
-    file:write_file(Path ++ "/.git/HEAD", <<"ref: refs/heads/", (list_to_binary(Branch))/binary>>).
+    ok = file:write_file(Path ++ "/.git/HEAD", 
+        <<"ref: refs/heads/", (list_to_binary(Branch))/binary>>),
+    {ok, BranchHash} = file:read_file(Path ++ "/.git/refs/heads/" ++ Branch),
+    <<BranchDir:2/binary, BranchFileName:38/binary, _/binary>> = BranchHash,
+    PathBin = list_to_binary(Path),
+    {ok, BranchFile} = file:read_file(<<PathBin/binary, "/.git/objects/", BranchDir/binary, "/", BranchFileName/binary>>),
+    UnzippedBranch = zlib:uncompress(BranchFile),
+    [_Commit, <<"tree ", TreeDir:2/binary, TreeFileName:38/binary, _BranchInfo/binary>>] = binary:split(UnzippedBranch, [<<0>>]),
+    {ok, TreeFile} = file:read_file(<<PathBin/binary, "/.git/objects/", TreeDir/binary, "/", TreeFileName/binary>>),
+    UnzippedTree = zlib:uncompress(TreeFile),
+    [Tree, TreeData] = binary:split(UnzippedTree, [<<0>>]),
+    checkout(Tree, TreeData, PathBin, PathBin).
+
+checkout(<<"blob", _Rest/binary>>, Data, _Path, CurrentPath) ->
+    ok = file:write_file(CurrentPath, Data),
+    ok;
+checkout(<<"tree", _TreeId/binary>>, <<>>, _Path, _CurrentPath) ->
+    ok;
+checkout(<<"tree", TreeId/binary>>, Data, Path, CurrentPath) ->
+    ok = filelib:ensure_dir(<<CurrentPath/binary, "/">>),
+    [FileId, <<FileHash:20/binary, Rest/binary>>] = binary:split(Data, [<<0>>]),
+    <<Dir:2/binary, File:38/binary>> = list_to_binary(lists:flatten([io_lib:format("~2.16.0b",[X]) || <<X:8>> <= FileHash])),
+    [_Id, FileName] = binary:split(FileId, [<<" ">>]),
+    {ok, ZipFile} = file:read_file(<<Path/binary, "/.git/objects/", Dir/binary, "/", File/binary>>),
+    UnzipFile = zlib:uncompress(ZipFile),
+    [Type, FileData] = binary:split(UnzipFile, [<<0>>]),
+    case file:read_file(<<CurrentPath/binary, "/", FileName/binary>>) of
+        {ok, FileRepoData} ->
+            CurrentHash = lists:flatten([io_lib:format("~2.16.0b",[X]) || <<X:8>> <= crypto:hash(sha, <<Type/binary, "\0", FileRepoData/binary>>)]),
+            case <<Dir:2/binary, File:38/binary>> == CurrentHash of
+                true ->
+                    ok;
+                false ->
+                    ok = checkout(Type, FileData, Path, <<CurrentPath/binary, "/", FileName/binary>>)
+            end;
+        _ ->
+            ok = checkout(Type, FileData, Path, <<CurrentPath/binary, "/", FileName/binary>>)
+    end,
+    checkout(<<"tree", TreeId/binary>>, Rest, Path, CurrentPath).
 
 merge(Repository, Branch) ->
     {ok, Dir} = application:get_env(gel, repos_dir),
